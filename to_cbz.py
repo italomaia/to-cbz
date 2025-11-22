ac6c252e4ce7fb4f1fb0387996eede7080578226
Italo Maia <italo.maia@gmail.com>
2025-11-22 20:35:11 +0100

    Added type hints

--- to_cbz.py
+++ to_cbz.py
@@ -5,28 +5,29 @@ import zipfile
 from multiprocessing import Pool
 
 
-def make_filename(dirname, ext):
+def make_filename(dirname: str, ext: str):
     if dirname.endswith("/"):
         dirname = dirname[:-1]
     return "%s.%s" % (dirname, ext)
 
 
-def to_cbz(dirname):
-    cbz_filename = make_filename(dirname, 'cbz')
+def to_cbz(dirname: str):
+    cbz_filename = make_filename(dirname, "cbz")
     zfile = zipfile.ZipFile(cbz_filename, "w")
 
-    for rootpath, dirnames, filenames in os.walk(dirname):
+    for rootpath, _, filenames in os.walk(dirname):
         for filename in filenames:
             abs_filename = os.path.join(rootpath, filename)
             zfile.write(abs_filename)
     zfile.close()
 
 
-def main(args):
+def main(args: list[str]):
     pool = Pool()
+
     for arg in args:
         if os.path.isdir(arg):
-            pool.apply_async(to_cbz, [arg], callback=lambda r:None)
+            _ = pool.apply_async(to_cbz, [arg], callback=lambda r: None)
         else:
             print(arg, "is a file. Ignoring.")
 
@@ -39,4 +40,5 @@ def main(args):
 
 if __name__ == "__main__":
     import sys
+
     main(sys.argv[1:])
